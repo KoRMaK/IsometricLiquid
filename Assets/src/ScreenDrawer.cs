40c35c57ef34ae3c7b051ff1076c8aad1dc93df6
Corey P. Fleming <coreypfleming@gmail.com>
2015-09-30 23:47:50 -0400

    Add code to draw stuff on a texture corresponding to a cubes location

--- Assets/src/ScreenDrawer.cs
+++ Assets/src/ScreenDrawer.cs
@@ -5,6 +5,7 @@ public class ScreenDrawer : MonoBehaviour {
 
 	public Texture2D tex;
 	public int tex_size = 20;
+	public GameObject some_cube;
 	
 	// Use this for initialization
 	void Start () {
@@ -28,7 +29,20 @@ public class ScreenDrawer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		Camera cam = GetComponent<Camera>();
+		
+		Vector3 screen_coords = cam.WorldToScreenPoint(some_cube.transform.position);
+		
+		for(int i = 0; i < 20; i++)
+		{
+			for(int _x = 10; _x < 20; _x++)
+			{ 
+				tex.SetPixel((int)screen_coords.x + i, ((int)screen_coords.y) + _x, Color.cyan);
+			}
+		}
+		tex.Apply();
+		
 	}
 	
 	void OnGUI() {
