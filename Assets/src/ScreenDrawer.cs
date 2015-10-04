5e220897a8e8f860c5fc4fdf8a33f5f8552244c8
Corey P. Fleming <coreypfleming@gmail.com>
2015-10-03 22:11:45 -0400

    Add a workling implementation of marching cubes

--- Assets/src/ScreenDrawer.cs
+++ Assets/src/ScreenDrawer.cs
@@ -6,10 +6,81 @@ public class ScreenDrawer : MonoBehaviour {
 	public Texture2D tex;
 	public int tex_size = 20;
 	public GameObject some_cube;
+	//create a 16x16 array of booleans
+	public bool[,] bool_values = new bool[16,16];
+	public Vector2[][] marching_cube_templates = new Vector2[16][];
 	
 	// Use this for initialization
 	void Start () {
+	
 		tex = new Texture2D(Screen.width, Screen.height);
+		
+		marching_cube_templates[0] = null; //new 2(0, 0);
+		marching_cube_templates[1] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 0.0f)};
+		marching_cube_templates[2] = new Vector2[] {new Vector2(0.5f, 0.0f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[3] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[4] = new Vector2[] {new Vector2(0.5f, 1.0f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[5] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 1.0f), new Vector2(0.5f, 0.0f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[6] = new Vector2[] {new Vector2(0.5f, 0.0f), new Vector2(0.5f, 1.0f)};
+		marching_cube_templates[7] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 1.0f)};
+		marching_cube_templates[8] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 1.0f)};
+		marching_cube_templates[9] = new Vector2[] {new Vector2(0.5f, 0.0f), new Vector2(0.5f, 1.0f)};
+		marching_cube_templates[10] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 0.0f), new Vector2(0.5f, 1.0f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[11] = new Vector2[] {new Vector2(0.5f, 1.0f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[12] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[13] = new Vector2[] {new Vector2(0.5f, 0.0f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[14] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 0.0f)};
+		marching_cube_templates[15] = new Vector2[] {new Vector2(0.0f, 0.0f), new Vector2(1.0f, 0.0f), new Vector2(0.0f, 0.0f), new Vector2(1.0f, 0.0f), new Vector2(0.0f, 0.2f), new Vector2(1.0f, 0.2f), new Vector2(0.0f, 0.5f), new Vector2(1.0f, 0.5f), new Vector2(0.0f, 0.7f), new Vector2(1.0f, 0.7f), new Vector2(0.0f, 1.0f), new Vector2(1.0f, 1.0f)}; //new Vector2[] {new Vector2(0.0f, 0.0f), new Vector2(1.0f, 1.0f), new Vector2(0.0f, 1.0f), new Vector2(1.0f, 0.0f)};
+		
+		for(int i = 0; i<  16; i++)
+		{
+			for(int j = 0; j<  16; j++)
+			{
+				bool_values[i, j] =  false;
+				
+			}
+		}
+		
+		for(int i = 1; i<  15; i++)
+		{
+			for(int j = 1; j<  15; j++)
+			{
+				bool_values[i, j] =  (Random.value > 0.5f) ;
+			
+			}
+		}
+		
+		//now that the values are set, lets draw them
+		for(int i = 0; i<  15; i++)
+		{
+			for(int j = 0; j < 15; j++)
+			{
+				int index = 0;
+				if(bool_values[i,j])
+					index += 1;
+				if(bool_values[i+1,j])
+					index += 2;
+				if(bool_values[i+1, j+1])
+					index += 4;
+				if(bool_values[i, j+1])
+					index += 8;
+					
+				int _multi_val_x = Screen.width / 15;
+				int _multi_val_y = Screen.height / 15;
+				//ok we got our index, now draw it
+				if(marching_cube_templates[index] != null)
+				{
+					for(int idx = 0; idx + 1< marching_cube_templates[index].Length; idx += 2)
+					{
+						TextureDraw.DrawLine(tex, (int)(marching_cube_templates[index][idx].x * _multi_val_x) + (i * _multi_val_x), (int)(marching_cube_templates[index][idx].y * _multi_val_y) + (j * _multi_val_y), (int)(marching_cube_templates[index][idx + 1].x * _multi_val_x) + (i * _multi_val_x), (int)(marching_cube_templates[index][idx + 1].y * _multi_val_y) + (j * _multi_val_y), Color.cyan);
+					}
+				}
+					
+			}
+		}
+		
+		/*
+		
 		for(int i = 0; i < Screen.width; i++)
 		{
 			for(int _x = 10; _x < 20; _x++)
@@ -23,6 +94,7 @@ public class ScreenDrawer : MonoBehaviour {
 		tex.SetPixel(2, 3, Color.cyan);
 		tex.SetPixel(3, 3, Color.cyan);
 		tex.SetPixel(4, 3, Color.cyan);
+		*/
 		tex.Apply();
 		
 	}
@@ -33,7 +105,7 @@ public class ScreenDrawer : MonoBehaviour {
 		Camera cam = GetComponent<Camera>();
 		
 		Vector3 screen_coords = cam.WorldToScreenPoint(some_cube.transform.position);
-		
+		/*
 		for(int i = 0; i < 20; i++)
 		{
 			for(int _x = 10; _x < 20; _x++)
@@ -42,7 +114,7 @@ public class ScreenDrawer : MonoBehaviour {
 			}
 		}
 		tex.Apply();
-		
+		*/
 	}
 	
 	void OnGUI() {
