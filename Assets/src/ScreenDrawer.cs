a4e2d627a7df96ad4cd034a499bf99beaff790e9
Corey P. Fleming <coreypfleming@gmail.com>
2015-10-06 00:03:22 -0400

    Get marching cubes working properly with all the screen translations and shit. DrawLine converts screen space into texture space automatically. Fixed other things up. Needs optimized

--- Assets/src/ScreenDrawer.cs
+++ Assets/src/ScreenDrawer.cs
@@ -6,7 +6,7 @@ public class ScreenDrawer : MonoBehaviour {
 
 	public Texture2D tex;
 	public GameObject some_cube;
-	public static int resolution = 16;
+	public static int resolution = 32;
 	//create a 16x16 array of booleans
 	public bool[,] bool_values = new bool[resolution, resolution];
 	public Vector2[][] marching_cube_templates = new Vector2[16][];
@@ -19,6 +19,7 @@ public class ScreenDrawer : MonoBehaviour {
 		
 		cubes = new List<GameObject>();
 		tex = new Texture2D(Screen.width, Screen.height);
+		TextureDraw.ClearTexture(tex);
 		init_cube_templates();
 		init_terrain_data();
 		draw_marched_squares();
@@ -55,18 +56,13 @@ public class ScreenDrawer : MonoBehaviour {
 		Camera cam = GetComponent<Camera>();
 		
 		//Vector3 screen_coords = cam.WorldToScreenPoint(some_cube.transform.position);
-		/*
-		for(int i = 0; i < 20; i++)
-		{
-			for(int _x = 10; _x < 20; _x++)
-			{ 
-				tex.SetPixel((int)screen_coords.x + i, ((int)screen_coords.y) + _x, Color.cyan);
-			}
-		}
-		tex.Apply();
-		*/
+		TextureDraw.ClearTexture(tex);
 		
-		tex = new Texture2D(Screen.width, Screen.height);
+		//tex = new Texture2D(Screen.width, Screen.height);
+		//tex.SetPixels(new Color[] {Color.clear}, 0);
+		/*
+
+		*/
 		determine_terrain_data();
 		draw_marched_squares();
 		draw_cubes();
@@ -84,6 +80,9 @@ public class ScreenDrawer : MonoBehaviour {
 	void draw_marched_squares()
 	{
 	
+		int _multi_val_x = Screen.width / (resolution);
+		int _multi_val_y = Screen.height / (resolution);
+	
 				//now that the values are set, lets draw them
 		for(int i = 0; i<  resolution - 1; i++)
 		{
@@ -91,22 +90,21 @@ public class ScreenDrawer : MonoBehaviour {
 			{
 				int index = 0;
 				if(bool_values[i,j])
-					index += 1;
-				if(bool_values[i+1,j])
-					index += 2;
-				if(bool_values[i+1, j+1])
-					index += 4;
-				if(bool_values[i, j+1])
 					index += 8;
+				if(bool_values[i+1,j])
+					index += 4;
+				if(bool_values[i+1, j+1])
+					index += 2;
+				if(bool_values[i, j+1])
+					index += 1;
 					
-				int _multi_val_x = Screen.width / (resolution - 1);
-				int _multi_val_y = Screen.height / (resolution - 1);
+				
 				//ok we got our index, now draw it
 				if(marching_cube_templates[index] != null)
 				{
 					for(int idx = 0; idx + 1< marching_cube_templates[index].Length; idx += 2)
 					{
-						TextureDraw.DrawLine(tex, (int)(marching_cube_templates[index][idx].x * _multi_val_x) + (i * _multi_val_x), (int)(marching_cube_templates[index][idx].y * _multi_val_y) + (j * _multi_val_y), (int)(marching_cube_templates[index][idx + 1].x * _multi_val_x) + (i * _multi_val_x), (int)(marching_cube_templates[index][idx + 1].y * _multi_val_y) + (j * _multi_val_y), Color.cyan);
+						TextureDraw.DrawLine(tex, (int)(marching_cube_templates[index][idx].x * _multi_val_x) + (i * _multi_val_x) + (_multi_val_x/2), (int)(marching_cube_templates[index][idx].y * _multi_val_y) + (j * _multi_val_y) + (_multi_val_y/2), (int)(marching_cube_templates[index][idx + 1].x * _multi_val_x) + (i * _multi_val_x) + (_multi_val_x/2), (int)(marching_cube_templates[index][idx + 1].y * _multi_val_y) + (j * _multi_val_y) + (_multi_val_y/2), Color.cyan);
 					}
 				}					
 			
@@ -118,21 +116,21 @@ public class ScreenDrawer : MonoBehaviour {
 	void init_cube_templates()
 	{
 		marching_cube_templates[0] = null; //new 2(0, 0);
-		marching_cube_templates[1] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 0.0f)};
-		marching_cube_templates[2] = new Vector2[] {new Vector2(0.5f, 0.0f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[1] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 1.0f)};
+		marching_cube_templates[2] = new Vector2[] {new Vector2(0.5f, 1.0f), new Vector2(1.0f, 0.5f)};
 		marching_cube_templates[3] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(1.0f, 0.5f)};
-		marching_cube_templates[4] = new Vector2[] {new Vector2(0.5f, 1.0f), new Vector2(1.0f, 0.5f)};
-		marching_cube_templates[5] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 1.0f), new Vector2(0.5f, 0.0f), new Vector2(1.0f, 0.5f)};
-		marching_cube_templates[6] = new Vector2[] {new Vector2(0.5f, 0.0f), new Vector2(0.5f, 1.0f)};
-		marching_cube_templates[7] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 1.0f)};
-		marching_cube_templates[8] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 1.0f)};
-		marching_cube_templates[9] = new Vector2[] {new Vector2(0.5f, 0.0f), new Vector2(0.5f, 1.0f)};
-		marching_cube_templates[10] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 0.0f), new Vector2(0.5f, 1.0f), new Vector2(1.0f, 0.5f)};
-		marching_cube_templates[11] = new Vector2[] {new Vector2(0.5f, 1.0f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[4] = new Vector2[] {new Vector2(0.5f, 0.0f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[5] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 0.0f), new Vector2(0.5f, 1.0f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[6] = new Vector2[] {new Vector2(0.5f, 1.0f), new Vector2(0.5f, 0.0f)};
+		marching_cube_templates[7] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 0.0f)};
+		marching_cube_templates[8] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 0.0f)};
+		marching_cube_templates[9] = new Vector2[] {new Vector2(0.5f, 1.0f), new Vector2(0.5f, 0.0f)};
+		marching_cube_templates[10] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 1.0f), new Vector2(0.5f, 0.0f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[11] = new Vector2[] {new Vector2(0.5f, 0.0f), new Vector2(1.0f, 0.5f)};
 		marching_cube_templates[12] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(1.0f, 0.5f)};
-		marching_cube_templates[13] = new Vector2[] {new Vector2(0.5f, 0.0f), new Vector2(1.0f, 0.5f)};
-		marching_cube_templates[14] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 0.0f)};
-		marching_cube_templates[15] = new Vector2[] {new Vector2(0.0f, 0.0f), new Vector2(1.0f, 0.0f), new Vector2(0.0f, 0.0f), new Vector2(1.0f, 0.0f), new Vector2(0.0f, 0.2f), new Vector2(1.0f, 0.2f), new Vector2(0.0f, 0.5f), new Vector2(1.0f, 0.5f), new Vector2(0.0f, 0.7f), new Vector2(1.0f, 0.7f), new Vector2(0.0f, 1.0f), new Vector2(1.0f, 1.0f)}; //new Vector2[] {new Vector2(0.0f, 0.0f), new Vector2(1.0f, 1.0f), new Vector2(0.0f, 1.0f), new Vector2(1.0f, 0.0f)};
+		marching_cube_templates[13] = new Vector2[] {new Vector2(0.5f, 1.0f), new Vector2(1.0f, 0.5f)};
+		marching_cube_templates[14] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(0.5f, 1.0f)};
+		marching_cube_templates[15] = new Vector2[] {new Vector2(0.0f, 0.5f), new Vector2(1.0f, 0.5f)};//, new Vector2(0.0f, 0.2f), new Vector2(1.0f, 0.2f), new Vector2(0.0f, 0.7f), new Vector2(1.0f, 0.7f) }; //new Vector2[] {new Vector2(0.0f, 0.0f), new Vector2(1.0f, 1.0f), new Vector2(0.0f, 1.0f), new Vector2(1.0f, 0.0f)};
 		
 	}
 	
@@ -167,8 +165,8 @@ public class ScreenDrawer : MonoBehaviour {
 		//see if the cube is within the bounds of this cube, and if it is, set its terrain data to true.
 		//break out of the loop if the data is true for this cube
 		
-		int _multi_val_x = Screen.width / (resolution - 1);
-		int _multi_val_y = Screen.height / (resolution - 1);
+		int _multi_val_x = Screen.width / (resolution);
+		int _multi_val_y = Screen.height / (resolution );
 		
 		Camera cam = GetComponent<Camera>();
 		
@@ -183,29 +181,32 @@ public class ScreenDrawer : MonoBehaviour {
 			}
 		}
 		
-		
-		//randomize the inner ones
+
 		for(int i = 0; i<  resolution; i++)
 		{
 			for(int j = 0; j<  resolution; j++)
 			{
+				Rect _r0 = new Rect(new Vector2(i * _multi_val_x, j * _multi_val_y), new Vector2(_multi_val_x, _multi_val_y));
+				TextureDraw.DrawRectangle(tex, _r0, Color.blue);
 				
 				foreach(GameObject _cube in cubes)
 				{
+					if( bool_values[i, j] ==  true)
+						break;
 					
 					
 					Vector3 screen_coords = cam.WorldToScreenPoint(_cube.transform.position);
-
-					Rect _r0 = new Rect(new Vector2(i * _multi_val_x, j * _multi_val_y), new Vector2(_multi_val_x, _multi_val_y));
+					
 					Rect _r1 = RectangleCollisionChecker.BoundsToScreenRect(_cube.GetComponent<Renderer>().bounds);
-					_r1.y = Screen.height - _r1.y;
+					TextureDraw.DrawRectangle(tex, _r1, Color.yellow);
+					//_r1.y = Screen.height - _r1.y;
 					//Rect _r11 = new Rect(new Vector2(screen_coords.x, screen_coords.y), new Vector2(50, 50));
 					//Debug.Log(" bounds _r1 bounds " + _r1.ToString());
 					//Debug.Log(" bounds _r11 " + _r11.ToString());
 					if( RectangleCollisionChecker.intersects(_r0, _r1))
 					{
 						bool_values[i, j] =  true;
-						//break;
+						break;
 					}					
 					
 				}
@@ -221,11 +222,18 @@ public class ScreenDrawer : MonoBehaviour {
 		foreach (GameObject _cube in cubes) 
 		{
 			Rect _r1 = RectangleCollisionChecker.BoundsToScreenRect (_cube.GetComponent<Renderer> ().bounds);
-			TextureDraw.DrawLine(tex, (int)(_r1.x), (int)(Screen.height - _r1.y), (int)(_r1.xMax), (int)(Screen.height - _r1.y), Color.cyan);
+			Rect _r1_tex = new Rect(_r1.x, _r1.y, _r1.width, _r1.height);
+			//TextureDraw.DrawRectangle(tex, _r1_tex, Color.green);
+			TextureDraw.DrawLine(tex, (int)(_r1.x), (int)(_r1.y), (int)(_r1.xMax), (int)(_r1.y), Color.cyan);
 			//Rect _r2 = RectangleCollisionChecker.BoundsToScreenRect(_cube.GetComponent<Renderer>().bounds);
-			TextureDraw.DrawLine(tex, (int)(_r1.x), (int)(Screen.height - _r1.y), (int)(_r1.xMax), (int)(Screen.height - _r1.yMax), Color.cyan);
+			TextureDraw.DrawLine(tex, (int)(_r1.x), (int)(_r1.y), (int)(_r1.xMax), (int)(_r1.yMax), Color.cyan);
 		}
 		tex.Apply();
+	}
+	
+	float ScreenYToTextureY(float screen_y)
+	{
+		return Screen.height - screen_y;
 	}
 	
 }
--- Assets/src/TextureDraw.cs
+++ Assets/src/TextureDraw.cs
@@ -5,6 +5,9 @@ public class TextureDraw {
 
 	public static void DrawLine(Texture2D tex, int x0, int y0, int x1, int y1, Color col)
 	{
+		//first, lets flop the y's because they shuld be in screen space
+		y0 = Screen.height - y0;
+		y1 = Screen.height - y1;
 		int dy = (int)(y1-y0);
 		int dx = (int)(x1-x0);
 		int stepx, stepy;
@@ -41,6 +44,35 @@ public class TextureDraw {
 				y0 += stepy;
 				fraction += dx;
 				tex.SetPixel(x0, y0, col);
+			}
+		}
+	}
+	
+	public static void DrawLine(Texture2D tex, float x0, float y0, float x1, float y1, Color col)
+	{
+		DrawLine(tex, (int)x0, (int)y0, (int)x1, (int)y1, col);
+	}
+	
+	public static void DrawRectangle(Texture2D tex, Rect rect, Color color)
+	{
+		//top left -> top right
+		//top left -> bottom left
+		//top right -> bottom right
+		//bottom left -> bottom right
+		DrawLine(tex, rect.xMin, rect.yMin, rect.xMax, rect.yMin, color);
+		DrawLine(tex, rect.xMin, rect.yMin, rect.xMin, rect.yMax, color);
+		DrawLine(tex, rect.xMax, rect.yMin, rect.xMax, rect.yMax, color);
+		DrawLine(tex, rect.xMin, rect.yMax, rect.xMax, rect.yMax, color);
+		
+	}
+	
+	public static void ClearTexture(Texture2D tex)
+	{
+		for(int x = 0; x < Screen.width; x++)
+		{
+			for(int y = 0; y < Screen.height; y++)
+			{
+				tex.SetPixel(x, y, Color.clear);
 			}
 		}
 	}
