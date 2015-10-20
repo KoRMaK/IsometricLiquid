4c16c2ed4ca7258f1b7397953305651eab0e5ec0
Corey P. Fleming <coreypfleming@gmail.com>
2015-10-19 20:39:52 -0400

    Add Cricle class

---
+++ Assets/src/Circle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Circle {
+	public Vector2 center;
+	public float radius;
+
+	public Circle(Vector2 _center, float _radius)
+	{
+		center = _center;
+		radius = _radius;
+	}
+}
